90cd34d1783644fe7b7c460c1c596d74207cc6a7
Zhiwei Li <lzwjava@gmail.com>
2025-02-13 18:36:42 +0800

    chore(server): update bandwidth_auto.sh

--- server/bandwidth_auto.sh
+++ server/bandwidth_auto.sh
@@ -1,8 +1,6 @@
-sudo apt-get update
-sudo apt-get install vnstat
-sudo apt install python3-pip
-pip install Flask
-sudo apt install emacs-nox
-pip3 install flask_cors
-sudo apt install nginx
+sudo apt-get update -y
+sudo apt-get install vnstat -y
+sudo apt install python3-pip -y
+sudo apt install -y --force-yes emacs-nox
+sudo apt install nginx -y
 sudo apt install certbot python3-certbot-nginx -y
