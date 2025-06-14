d36967fbc25938414c3ce1dd2c64ce878c40ece9
lzwjava <lzwjava@gmail.com>
2025-06-14 22:41:32 +0800

    fix(build): remove unused application.yaml

--- src/main/java/org/lzwjava/DebugAspect.java
+++ src/main/java/org/lzwjava/DebugAspect.java
@@ -1,8 +1,9 @@
 package org.lzwjava;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -12,8 +13,13 @@ import org.springframework.stereotype.Component;
 public final class DebugAspect {
     private static final Logger log = LoggerFactory.getLogger(DebugAspect.class);
 
-    @After("execution(* org.lzwjava.HelloController.*(..))")
-    public void logAfter(JoinPoint joinPoint) {
+    @Before("execution(* org.lzwjava.HelloController.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
         log.info("Method executed: {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(pointcut = "execution(* org.lzwjava.HelloController.*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        log.info("Method {} returned: {}", joinPoint.getSignature(), result);
     }
 }
