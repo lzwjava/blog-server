fed16f37ccd0f9f9e480f5e5fd03c39562a6d9ae
lzwjava <lzwjava@gmail.com>
2025-06-15 01:36:03 +0800

    chore: rename HelloController to MainController

--- src/main/java/org/lzwjava/MainController.java
+++ src/main/java/org/lzwjava/MainController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class MainController {
 
     @CrossOrigin(origins = "*")
     @GetMapping("/bandwidth")
